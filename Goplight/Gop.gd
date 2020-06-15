c77fa4114ae1bd8b48147f2aa5c9f66e147510d3
Rockython <61116928+Rockython@users.noreply.github.com>
2020-06-16 01:17:18 +0700

    Animation MoveLeft Move Right
    Animation MoveLeft Move Right was added. Also it includes other animation, but i have to fix it

--- Goplight/Gop.gd
+++ Goplight/Gop.gd
@@ -3,7 +3,10 @@ extends KinematicBody2D
 const MAX_SPEED = 30
 const ACCELERATION = 150
 const FRICTION = 175
+
 var velocity = Vector2.ZERO
+
+onready var animationHero = $AnimationHero
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -13,9 +16,11 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		if input_vector.x > 0:
+			animationHero.play("MoveRight")
+		else:
+			animationHero.play("MoveLeft")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, delta * ACCELERATION)
-		# velocity += input_vector * ACCELERATION * delta 
-		# velocity = velocity.clamped(MAX_SPEED * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, delta * FRICTION)
 	velocity = move_and_slide(velocity)
