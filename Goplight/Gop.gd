0571d1a2d43e53cd16419a33b2f5ebe41f14c725
Rockython <61116928+Rockython@users.noreply.github.com>
2020-06-16 14:20:09 +0700

    Fixes of Animation

--- Goplight/Gop.gd
+++ Goplight/Gop.gd
@@ -1,12 +1,14 @@
 extends KinematicBody2D
 
-const MAX_SPEED = 30
-const ACCELERATION = 150
-const FRICTION = 175
+const MAX_SPEED = 20
+const ACCELERATION = 100
+const FRICTION = 120
 
 var velocity = Vector2.ZERO
 
 onready var animationHero = $AnimationHero
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -16,12 +18,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		if input_vector.x > 0:
-			animationHero.play("MoveRight")
-		else:
-			animationHero.play("MoveLeft")
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Move/blend_position", input_vector)
+		animationState.travel("Move")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, delta * ACCELERATION)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, delta * FRICTION)
 	velocity = move_and_slide(velocity)
 
